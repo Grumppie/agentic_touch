bc84ce994ef6581ad8cffde6cb6eddfe0fd4d185
grummpie <sarthakrajesh777@gmail.com>
2026-01-19 13:45:28 +0530

    06 error-tracking

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { demoGenerate } from "@/inngest/function";
+import { demoError, demoGenerate } from "@/inngest/function";
 
 export const runtime = "nodejs";
 
@@ -8,6 +8,7 @@ export const runtime = "nodejs";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    demoGenerate, // <-- This is where you'll always add all your functions
+    demoGenerate,
+    demoError
   ],
 });
--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,8 +1,12 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import * as Sentry from "@sentry/nextjs"
+import { useAuth } from "@clerk/nextjs";
 
 export default function Demo(){
+
+    const {userId} = useAuth()
 
     const [loading,setLoading] = useState(false);
     const [loadingBg,setLoadingBg] = useState(false);
@@ -20,13 +24,36 @@ export default function Demo(){
         setLoadingBg(false)
     }
 
+    
+    const handleClientError = ()=>{
+        Sentry.logger.info("User attempting to click on client Function",{userId})
+        throw new Error("Client Error: Something went wrong")
+    }
+
+    const handleAPIError = async()=>{
+        await fetch('/api/demo/error',{method:"POST"})
+    }
+
+    const handleInggestError = async()=>{
+        await fetch('/api/demo/inngest-error',{method: "POST"})
+    }
+
     return(
-        <div className="bg-amber-200">
-            <Button variant={"destructive"} onClick={handleBlocking} disabled={loading}>
+        <div className="p-2 bg-card-foreground rounded flex gap-2 flex-col max-w-fit">
+            <Button variant={"secondary"} onClick={handleBlocking} disabled={loading}>
                 {loading? "loading...":"Blocking"}
             </Button>
-            <Button variant={"destructive"} onClick={handleBackground} disabled={loadingBg}>
-                {loading? "loading...":"Background"}
+            <Button variant={"secondary"} onClick={handleBackground} disabled={loadingBg}>
+                {loadingBg? "loading...":"Background"}
+            </Button>
+            <Button variant={"destructive"} onClick={handleClientError} disabled={loadingBg}>
+                Client Error
+            </Button>
+            <Button variant={"destructive"} onClick={handleAPIError} disabled={loadingBg}>
+                API Error
+            </Button>
+            <Button variant={"destructive"} onClick={handleInggestError} disabled={loadingBg}>
+                Inggest Error
             </Button>
         </div>
     )
